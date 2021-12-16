b80f3e8dbf94fe71d2ed2a3292ac3bf4c4fd41d6
allan.ponchaut <allan_ponchaut@ens.univ-artois.fr>
2021-12-16 16:46:55 +0100

    vidéo sur page d'accueil

--- resources/views/components/episode.blade.php
+++ resources/views/components/episode.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div style="margin-left: 10px;margin-right: 100px;border-radius: 25px;border: 1px #455d5d">
         <img style="width:125px;float: left;border-radius: 10px;border: 1px;border-color: #1a202c;height:70px " src="{{asset($ep->urlImage) }}" >
-        <p style="float: left;margin-left: 10px">{{$ep->nom}}<br> Episode n°{{$ep->numero}} Saison {{$ep->saison}} Durée:{{$ep->duree}} min</p>
+        <p style="float: left;margin-left: 10px">{{$ep->nom}}<br> Episode n°{{$ep->numero}}, saison {{$ep->saison}} Durée:{{$ep->duree}} min</p>
         <p style="float: right;margin-left: 10px">sorti le: {{$ep->premiere}}</p>
         @guest()
         @else
